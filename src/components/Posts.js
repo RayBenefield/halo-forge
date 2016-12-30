a618d660e1c1fb4782c0e05841591ffcf15acdf9
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-12-29 17:10:11 -0800

    Changed the structure of the card with more useful information.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Card, CardTitle } from 'material-ui/Card';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 
 export default ({ posts }) => (
     <div>
-        {posts.map((post, i) =>
-            <Card key={i}>
-                <CardTitle title={post.title} />
-            </Card>
-        )}
+        {posts.map((post, i) => {
+            const link = (<a href={post.url}>{post.title}</a>);
+            const image = post.preview ? post.preview.images[0].resolutions[0].url.replace(/&amp;/g, '&') : 'http://lorempixel.com/108/81/';
+            return (
+                <Card key={i}>
+                    <CardHeader
+                        title={link}
+                        subtitle={post.author}
+                        actAsExpander
+                        showExpandableButton
+                    >
+                        <img src={image} alt={post.title} height={81} width={101} />
+                    </CardHeader>
+                    <CardText expandable>
+                        {post.selftext}
+                    </CardText>
+                </Card>
+            );
+        })}
     </div>
 );
