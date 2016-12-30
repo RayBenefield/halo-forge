130e10377910db11a32a8c58e99603d587aed5f1
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-12-30 10:37:20 -0800

    Changed out standard HTML for Material UI elements in the Picker.

--- src/components/Picker/index.js
+++ src/components/Picker/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import equip from './equip';
 
 const Picker = ({ value, onChange, options }) => (
     <span>
-        <select
-            onChange={e => onChange(e.target.value)}
+        <SelectField
+            floatingLabelText="Subreddit"
+            onChange={(e, i, v) => onChange(v)}
             value={value}
         >
             {options.map(option =>
-                <option value={option} key={option}>
-                    {option}
-                </option>)
-            }
-        </select>
+                <MenuItem value={option} primaryText={option} key={option} />
+            )}
+        </SelectField>
     </span>
 );
 
