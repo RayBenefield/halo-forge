1a3ca5edc83a522683db1630f11af974bfe3cdce
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-12-30 07:46:18 -0800

    Applied the dark theme for material ui.

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import configureStore from '../configureStore';
 import AsyncApp from './AsyncApp';
 
@@ -8,7 +10,7 @@ const store = configureStore();
 
 export default () => (
     <Provider store={store}>
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
             <AsyncApp />
         </MuiThemeProvider>
     </Provider>
