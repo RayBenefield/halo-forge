3ff55b3cb8f118355665c9ef903c031a110ed55c
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-12-30 08:27:16 -0800

    Adjusted the layout of the post cards.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,19 +4,19 @@ import { Card, CardHeader, CardText } from 'material-ui/Card';
 export default ({ posts }) => (
     <div>
         {posts.map((post, i) => {
-            const link = (<a href={post.url}>{post.title}</a>);
+            const link = (<sub style={{ float: 'right' }}><a href={post.url}>/r/halo • 22 hours ago</a></sub>);
             const image = post.preview ? post.preview.images[0].resolutions[0].url.replace(/&amp;/g, '&') : 'http://lorempixel.com/108/81/';
             return (
-                <Card key={i}>
+                <Card key={i} style={{ width: '320px' }}>
+                    {link}
+                    <img src={image} alt={post.title} height={81} width={101} style={{ float: 'left', padding: '8px' }} />
                     <CardHeader
-                        title={link}
+                        title={post.title}
                         subtitle={post.author}
                         actAsExpander
                         showExpandableButton
-                    >
-                        <img src={image} alt={post.title} height={81} width={101} />
-                    </CardHeader>
-                    <CardText expandable>
+                    />
+                    <CardText actAsExpander expandable>
                         {post.selftext}
                     </CardText>
                 </Card>
