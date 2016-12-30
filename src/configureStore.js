9b3d77bd96cd4116ac8362bedacd83cf0b5d059d
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-12-30 08:12:01 -0800

    Setup Redux Dev Tools.

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const loggerMiddleware = createLogger();
 
 export default function configureStore(preloadedState) {
     return createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(
+        composeEnhancers(applyMiddleware(
             thunkMiddleware,
             loggerMiddleware
-        ),
+        )),
     );
 }
