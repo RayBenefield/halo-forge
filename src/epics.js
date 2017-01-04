9bd102cff0b65fabbaf66a550d8bf60398d35570
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-04 07:03:02 -0800

    Setup the call to reddit.

--- src/epics.js
+++ src/epics.js
@@ -1,8 +1,16 @@
-import 'rxjs/add/operator/delay';
+import fetch from 'rxjs-fetch';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mapTo';
 import { REQUEST_POSTS, RECEIVE_POSTS } from './actions';
+import parsers from './parsers';
 
 export default action$ =>
     action$.ofType(REQUEST_POSTS)
-        .delay(1000)
-        .mapTo({ type: RECEIVE_POSTS, posts: [] });
+        .switchMap(action =>
+            fetch(`https://www.reddit.com/r/${action.subreddit}.json`)
+                .json()
+                .map(json => ({
+                    type: RECEIVE_POSTS,
+                    posts: parsers[action.source](action.subreddit, json),
+                }))
+        );
---
+++ src/parsers.js
@@ -0,0 +1,30 @@
+import _ from 'underscore';
+import uuid from 'node-uuid';
+import { NEW, ADDED, DROPPED } from './actions';
+import rHaloImage from './r-halo-image.png';
+import rHaloSource from './r-halo.png';
+
+export default {
+    reddit: (subreddit, results) => results.data.children.map((child) => {
+        const post = child.data;
+        // eslint-disable-next-line no-nested-ternary
+        const image = post.preview
+            ? (post.preview.images[0].resolutions.length > 0
+                ? post.preview.images[0].resolutions[0].url.replace(/&amp;/g, '&')
+                : post.preview.images[0].source.url
+            )
+            : rHaloImage;
+        return {
+            id: uuid.v4(),
+            status: [NEW, ADDED, DROPPED][_.random(0, 2)],
+            url: post.url,
+            title: post.title,
+            image,
+            source: `/r/${subreddit}`,
+            sourceId: post.id,
+            sourceImage: rHaloSource,
+            sourceUrl: `https://www.reddit.com/${post.permalink}`,
+            added: post.created_utc * 1000,
+        };
+    }),
+};
