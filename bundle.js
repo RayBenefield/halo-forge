550fa3853895cecbc9daae963a03141e153572ae
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-06 03:45:41 -0800

    Updates

--- bundle.js
+++ bundle.js
@@ -70775,7 +70775,7 @@
 	            { style: { position: 'relative', display: 'inline-block' }, onTouchMove: this.move, onTouchEnd: this.swiped, onTouchStart: this.start },
 	            _react2.default.createElement(
 	                _Card.Card,
-	                { style: _underscore2.default.extend({ marginLeft: left + 'px', transition: transition, opacity: opacity }, _underscore2.default.omit(style, 'opacity')) },
+	                { style: _underscore2.default.extend({ marginLeft: left + 'px', transition: transition }, _underscore2.default.omit(style, 'opacity')) },
 	                _react2.default.createElement(
 	                    'a',
 	                    { href: post.url },
