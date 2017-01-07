f578d9547008ba34117fb875f7c5036cf716b55b
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-07 04:08:14 -0800

    Added back online functionality now that it is the weekend.

--- src/epics.js
+++ src/epics.js
@@ -3,29 +3,29 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mapTo';
 import { REQUEST_POSTS, RECEIVE_POSTS } from './actions';
 import parsers from './parsers';
-import json1 from '../data/reddit-forge-1.json';
-import json2 from '../data/reddit-forge-2.json';
+//import json1 from '../data/reddit-forge-1.json';
+//import json2 from '../data/reddit-forge-2.json';
 
-let calledAlready = false;
+//let calledAlready = false;
 export default action$ =>
     action$.ofType(REQUEST_POSTS)
-        .map((action) => {
-            const newAction = {
-                type: RECEIVE_POSTS,
-                source: action.source,
-                subreddit: action.subreddit,
-                posts: parsers[action.source](action.subreddit, calledAlready ? json2 : json1),
-            };
-            calledAlready = !calledAlready;
-            return newAction;
-        });
-        //.switchMap(action =>
-            //fetch(`https://www.reddit.com/r/${action.subreddit}.json`)
-                //.json()
-                //.map(json => ({
-                    //type: RECEIVE_POSTS,
-                    //source: action.source,
-                    //subreddit: action.subreddit,
-                    //posts: parsers[action.source](action.subreddit, json),
-                //}))
-        //);
+        //.map((action) => {
+            //const newAction = {
+                //type: RECEIVE_POSTS,
+                //source: action.source,
+                //subreddit: action.subreddit,
+                //posts: parsers[action.source](action.subreddit, calledAlready ? json2 : json1),
+            //};
+            //calledAlready = !calledAlready;
+            //return newAction;
+        //});
+        .switchMap(action =>
+            fetch(`https://www.reddit.com/r/${action.subreddit}.json`)
+                .json()
+                .map(json => ({
+                    type: RECEIVE_POSTS,
+                    source: action.source,
+                    subreddit: action.subreddit,
+                    posts: parsers[action.source](action.subreddit, json),
+                }))
+        );
