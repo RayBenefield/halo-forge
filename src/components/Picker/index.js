337f1e5eba313f3d701771da1d5a7ca744c91a0b
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-10 07:10:35 -0800

    Refactored the styling within the Picker component.

--- src/components/Picker/index.js
+++ src/components/Picker/index.js
@@ -4,19 +4,17 @@ import MenuItem from 'material-ui/MenuItem';
 import equip from './equip';
 
 const Picker = ({ value, onChange, options }) => (
-    <div style={{ width: '100%' }}>
-        <SelectField
-            onChange={(e, i, v) => onChange(v)}
-            value={value}
-            labelStyle={{ paddingLeft: '16px' }}
-            iconStyle={{ paddingRight: '16px' }}
-            fullWidth
-        >
-            {options.map(option =>
-                <MenuItem value={option} primaryText={option} key={option} />
-            )}
-        </SelectField>
-    </div>
+    <SelectField
+        onChange={(e, i, v) => onChange(v)}
+        value={value}
+        labelStyle={{ paddingLeft: '16px' }}
+        iconStyle={{ paddingRight: '16px' }}
+        fullWidth
+    >
+        {options.map(option =>
+            <MenuItem value={option} primaryText={option} key={option} />
+        )}
+    </SelectField>
 );
 
 export default equip(Picker);
