d123bb1b967a372fbeb2c39b8ac25891dcf8569e
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-14 05:25:16 -0800

    Setup a Picker placeholder that removes material-ui from Picker.

--- src/components/Picker/index.js
+++ src/components/Picker/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
+import classes from 'classnames';
 import equip from './equip';
 
-const Picker = ({ value, onChange, options }) => (
-    <SelectField
-        onChange={(e, i, v) => onChange(v)}
-        value={value}
-        labelStyle={{ paddingLeft: '16px' }}
-        iconStyle={{ paddingRight: '16px' }}
-        fullWidth
-    >
-        {options.map(option =>
-            <MenuItem value={option} primaryText={option} key={option} />
-        )}
-    </SelectField>
-);
+const Picker = React.createClass({
+    getInitialState() {
+        return { show: false };
+    },
+    show() {
+        this.setState({ show: !this.state.show });
+    },
+    render() {
+        const { value, onChange, options } = this.props;
+        return (
+            <div className={classes("w-100", "bg-grey-800", "roboto")} onClick={this.show}>
+                <div className={classes("pa2", "pl3")}>{value}</div>
+                <hr />
+                <div className={classes("di")}>
+                    {this.state.show && options.map(option =>
+                        <div className={classes("pa2", "pl3")} onClick={(e) => console.log(e)}>{option}</div>
+                    )}
+                </div>
+            </div>
+        );
+    },
+});
 
 export default equip(Picker);
