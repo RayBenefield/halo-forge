168ad4769ffaee5157dcec4bbe8f36fd0143e7cf
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-14 05:41:45 -0800

    Got rid of the need for the material-ui AppBar.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
+import classes from 'classnames';
 import Refresher from './Refresher';
 import Filter from './Filter';
 
 const Header = () => (
-    <AppBar
-        title="Halo Forge"
-        iconElementLeft={<div />}
-        iconElementRight={
-            <div>
-                <Filter />
-                <Refresher />
-            </div>
-        }
-    />
+    <div className={classes("bg-grey-900")} style={{ height: '64px' }}>
+        <div className={classes("f2", "roboto", "di", "pa4", "white-text")}>Halo Forge</div>
+        <Filter />
+        <Refresher />
+    </div>
 );
 
 export default Header;
