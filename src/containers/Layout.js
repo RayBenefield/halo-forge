bdebe7e0ccc2719b7a7a5ba98fa5f6d6c48c7bcb
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-14 05:29:18 -0800

    Replaced Paper with a div to remove material-ui.

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classes from 'classnames'
 import { connect } from 'react-redux';
-import Paper from 'material-ui/Paper';
 import { fetchPosts } from 'src/actions';
 import Picker from 'src/components/Picker';
 import Header from 'src/components/Header';
@@ -13,9 +12,9 @@ const Layout = ({ dispatch, selectedSource }) => {
         <div className={classes("flex", "flex-column", "h-100")}>
             <div className={classes("h-7")}>
                 <Header />
-                <Paper className={classes("relative", "z-5" )} zDepth={4}>
+                <div className={classes("white-text", "relative", "z-5", "z-depth-4" )}>
                     <Picker />
-                </Paper>
+                </div>
             </div>
             <div className={classes("relative", "h-100", "overflow-x-hidden", "overflow-y-scroll", "flex-auto")}>
                 <Content />
