c8015427e7282139fc6ca5732bdc3f43c318afea
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-01-15 07:13:45 -0800

    Removed the need for material-ui period cutting a third of the package size.

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import configureStore from 'src/configureStore';
 import Layout from 'src/containers/Layout';
 
 const store = configureStore();
 
-const customDarkTheme = getMuiTheme(darkBaseTheme);
-customDarkTheme.textField.backgroundColor = '#424242';
-customDarkTheme.paper.backgroundColor = '#424242';
-customDarkTheme.appBar.color = '#212121';
-customDarkTheme.appBar.textColor = '#FFFFFF';
-
 export default () => (
     <Provider store={store}>
-        <MuiThemeProvider muiTheme={customDarkTheme}>
-            <Layout />
-        </MuiThemeProvider>
+        <Layout />
     </Provider>
 );
